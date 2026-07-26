76f7c15b8b2c14853a9354559f604e1c4eaea992
Mani Hamedani <mani.hamedani@icloud.com>
2026-07-25 18:14:43 -0700

    Add main screen and nav graph.

---
+++ app/src/main/java/com/example/sepotify/ui/main/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.sepotify.ui.main
+
+import androidx.lifecycle.ViewModel
+import com.example.sepotify.data.player.PlayerManager
+
+class MainViewModel(
+    val playerManager: PlayerManager
+) : ViewModel() {
+
+}
